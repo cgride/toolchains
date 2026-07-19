a1cde59221ade6341aabe90fef66cf40c77e6031
Gaspard Kirira <gaspardkirira9@gmail.com>
2026-07-19 13:27:43 +0300

    Bump version to 0.1.1

--- include/cgride/toolchains/version.hpp
+++ include/cgride/toolchains/version.hpp
@@ -33,7 +33,7 @@ namespace cgride::toolchains
   /**
    * @brief Patch version of the Cgride toolchains module.
    */
-  inline constexpr int version_patch = 0;
+  inline constexpr int version_patch = 1;
 
   /**
    * @brief Public API version of the Cgride toolchains module.
@@ -43,7 +43,7 @@ namespace cgride::toolchains
   /**
    * @brief Human-readable version string.
    */
-  inline constexpr std::string_view version_string = "0.1.0";
+  inline constexpr std::string_view version_string = "0.1.1";
 
 } // namespace cgride::toolchains
 
